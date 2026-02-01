a6eab0ccbfe6c88021851ad45ea7bfe2d9876a4d
Andrey Marchenko <andrei.marchenko@protonmail.com>
2026-02-01 20:15:56 +0100

    visitor pattern for operations

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -42,11 +42,16 @@ public class GenerateAst {
 
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
+
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
 
         writer.println("}");
         writer.close();
@@ -71,12 +76,44 @@ public class GenerateAst {
         }
         writer.println("        }");
 
+        // Visitor pattern
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println(
+            "        return visitor.visit" + className + baseName + "(this);"
+        );
+        writer.println("    }");
+
         // Fields
         writer.println();
         for (String field : fields) {
             String type = field.split(" ")[0];
             String name = field.split(" ")[1];
             writer.println("        final " + type + " " + name + ";");
+        }
+        writer.println("    }");
+        writer.println();
+    }
+
+    private static void defineVisitor(
+        PrintWriter writer,
+        String baseName,
+        List<String> types
+    ) {
+        writer.println("    interface Visitor<R> {");
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println(
+                "        R visit" +
+                    typeName +
+                    baseName +
+                    "(" +
+                    typeName +
+                    " " +
+                    baseName.toLowerCase() +
+                    ");"
+            );
         }
         writer.println("    }");
         writer.println();
