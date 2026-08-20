f39f4d2df2578162a04ce2e9d25fe61fe476e1f0
Andrey Marchenko <andrei.marchenko@protonmail.com>
2026-08-20 10:40:35 +0200

    finish first version of Interpreter

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -25,10 +25,68 @@ public class Interpreter implements Expr.Visitor<Object> {
         }
     }
 
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case TokenType.GREATER:
+                return (double)left > (double)right;
+            case TokenType.GREATER_EQUAL:
+                return (double)left >= (double)right;
+            case TokenType.LESS:
+                return (double)left < (double)right;
+            case TokenType.LESS_EQUAL:
+                return (double)left <= (double)right;
+            case TokenType.BANG_EQUAL:
+                return !isEqual(left, right);
+            case TokenType.EQUAL_EQUAL:
+                return isEqual(left, right);
+            case TokenType.MINUS:
+                return (double)left - (double)right;
+            case TokenType.SLASH:
+                return (double)left / (double)right;
+            case TokenType.STAR:
+                return (double)left * (double)right;
+            case TokenType.PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+
+                break;
+            default:
+                return null;
+        }
+
+        return null;
+    }
+
+    public Object visitConditionalExpr(Expr.Conditional expr) {
+        boolean condition = (boolean)evaluate(expr.condition);
+
+        if (condition) {
+            return evaluate(expr.thenBranch);
+        } else {
+            return evaluate(expr.elseBranch);
+        }
+    }
+
     private boolean isTruthy(Object object) {
         if (object == null) return false;
         if (object instanceof Boolean) return (boolean)object;
         return true;
+    }
+
+    private boolean isEqual(Object left, Object right) {
+        if (left == null && right == null) return true;
+        if (left == null) return false;
+
+        return left.equals(right);
     }
 
     private Object evaluate(Expr expr) {
